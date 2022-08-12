5a70b8a9b66d25d98e1430dac38c7577ea2556c3
Hardforehead <kochegar005@gmail.com>
2022-08-13 00:46:49 +0300

    feat: solution added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var a = [];
+	var b = {};
+	bracketsConfig.forEach(e => {a.push(e[0]); b[e[1]] = e[0];})
+	var s = [];
+	for (var i = 0; i < str.length; i++) {
+		var c = str[i];
+		((s.length !== 0) && (b[c] === s[s.length - 1])) ? s.pop() : s.push(c);
+	}
+	return s.length === 0;
 }
